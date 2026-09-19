eb0de47b24937e550053de1d5edfe6a57bab3e5d
Anant Saini <anantsaini.india@gmail.com>
2026-09-19 17:43:45 +0530

    initial commit

---
+++ src/main/java/com/personal/practice_jpa_specification/PracticeJpaSpecificationApplication.java
@@ -0,0 +1,13 @@
+package com.personal.practice_jpa_specification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJpaSpecificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeJpaSpecificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personal/practice_jpa_specification/PracticeJpaSpecificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.practice_jpa_specification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJpaSpecificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
